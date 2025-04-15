89cde23ef315061f4836a39f19bc85a61ad4109e
SanketASS20 <sanketsarvade20@gmail.com>
2025-04-15 20:07:05 +0530

    Update Additional.java
    Updated.

--- Additional.java
+++ Additional.java
@@ -1,20 +1,52 @@
+
+/*
+
+  Compiler :
+            1.	A Compiler takes the entire program in one go.
+            2.	It generates an intermediate machine code.
+            3.	It is best suited for production environment.
+            4.	Used for C, C++, C#, Scala, Java etc.
+
+  Interpreter :
+               1. An interpreter takes a single line of code at a time.
+               2. It never produces any intermediate machine code.
+               3. It is best suited for a software development environment.
+               4. Used for Python, PHP, Ruby etc.              
+                
+*/ 
+
+
 import java.util.Scanner;
-public class SumOfNumbers2
+ 
+public class Student
+
 {
-    public static void main(String args[])
-   { 
- i   int x, y, sum;
-     Scanner sc = new Scanner(System.in);
-     System.out.print("Enter the first number:");
-     x = sc.nextlnt();
-     System.out.print("Enter the second number:");
-     y = sc.nextlnt();
-     sum = sum(x,y);
-     System.out.println("The sum of two number x and y is:" + sum);
-    }
-    public static int sum(int a, int b)
-   {
-     int sum = a + b;
-     return sum;
-   }
+  String name;
+  int roll;
+  float marks;
+  
+  public void accept(){
+     Scanner in = new Scanner(System.in);
+     System.out.print("Enter student name:");    
+     name = in.nextLine();
+     System.out.print("Enter roll number:");
+     roll = in.nextlnt();
+     System.out.print("Enter marks:");
+     marks = in.nextFloat();
+    
+  }
+  public void display(){
+    System.out.println("Name:" + name);
+    System.out.println("Roll number:" + roll);
+    System.out.println("Marks:" + marks);
+    
+  }
+
+  public static void main(String args[]){
+    Student obj = new Student();
+    obj.accept();
+    obj.display();
+
+  }
+  
 }
