e014e3339340a7a09c2ac7598f24a244b9bf665b
SanketASS20 <sanketsarvade20@gmail.com>
2025-04-15 20:09:17 +0530

    Update AddSum.java
    Updated.

--- AddSum.java
+++ AddSum.java
@@ -1,55 +1,20 @@
 import java.util.Scanner;
-class Cab{
-    int fare;
-    int d;
-
-    public Cab(){
-        fare = 30;
-
+public class SumOfNumbers2
+{
+    public static void main(String args[])
+   { 
+ i   int x, y, sum;
+     Scanner sc = new Scanner(System.in);
+     System.out.print("Enter the first number:");
+     x = sc.nextlnt();
+     System.out.print("Enter the second number:");
+     y = sc.nextlnt();
+     sum = sum(x,y);
+     System.out.println("The sum of two number x and y is:" + sum);
     }
-
-    public Cab(int amt){
-        fare = amt;
-
-    }
-
-    void showFare(){
-        System.out.println("Total Fare: Rs" + fare);
-
-    }
-
-}
-
-class RideCab {
-    public static void main(String[] args){
-    int cd;
-    Scanner sc = new Scanner(Syystem.in);
-
-    System.out.print("Enter the cab distance from user:");
-    cd = sc.nextlnt();
-    if(cd>5){
-        Cab ob = new Cab (30 + 10 * (cd - 5));
-        System.out.print("Enter the distance travelled:");
-        ob.d = sc.nextlnt();
-        
-        ob.fare = ob.fare + 10 * ob.d;
-
-        ob.showFare();
-    }
-    else{
-        Cab ob = new Cab();
-        System.out.print("Enter the distance travelled:");
-        ob.d = sc.nextlnt();
-
-        ob.fare = ob.fare + 10 * ob.d;
-
-        ob.showFare();
-
-        Cab ob1 = new Cab(ob);
-        ob1.showFare();
-
-    } 
-
-    }
-
+    public static int sum(int a, int b)
+   {
+     int sum = a + b;
+     return sum;
+   }
 }
