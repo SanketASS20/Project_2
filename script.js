1045ff4b43e859981a324fc0ef486a2f3274d649
SanketASS20 <sanketsarvade20@gmail.com>
2025-06-01 22:33:01 +0530

    Update script.js
    Updated code.

--- script.js
+++ script.js
@@ -1,31 +1,43 @@
+import React from "react";
 
-let playerName = document.querySelectorAll("input")
-let btns = document.getElementsByClassName("diceRollButton")
-let scores = document.getElementsByTagName("span")
-
-// [b1,b2,n3,b4,b5,b6]
-
-// events,  addEventListener
-
-
-
-for(let i = 0; i<=btns.length-1; i++ ){
-   btns[i].addEventListener("click", ()=>{rollDice(btns[i].id)})
-}
-
-
-function rollDice(btn_id){
-
-   btns[btn_id].disabled = true
-   
-   let  arr= ["1", "2", "3", "4", "5", "6"]
-   // 0 - 5
-   let randomIndex = Math.floor(Math.random() * 6) // 0 - 5 only integers
-   let randomDice = arr[randomIndex]
-   // console.log(randomDice)
-   scores[btn_id].innerText = randomDice
- 
-}
-
-
-// random => 0<=x<1
+export const About = (props) => {
+  return (
+    <div id="about">
+      <div className="container">
+        <div className="row">
+          <div className="col-xs-12 col-md-6">
+            {" "}
+            <img src="img/about.jpg" className="img-responsive" alt="" />{" "}
+          </div>
+          <div className="col-xs-12 col-md-6">
+            <div className="about-text">
+              <h2>About Us</h2>
+              <p>{props.data ? props.data.paragraph : "loading..."}</p>
+              <h3>Why Choose Us?</h3>
+              <div className="list-style">
+                <div className="col-lg-6 col-sm-6 col-xs-12">
+                  <ul>
+                    {props.data
+                      ? props.data.Why.map((d, i) => (
+                          <li key={`${d}-${i}`}>{d}</li>
+                        ))
+                      : "loading"}
+                  </ul>
+                </div>
+                <div className="col-lg-6 col-sm-6 col-xs-12">
+                  <ul>
+                    {props.data
+                      ? props.data.Why2.map((d, i) => (
+                          <li key={`${d}-${i}`}> {d}</li>
+                        ))
+                      : "loading"}
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
